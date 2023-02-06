a9de8598575bcd06e38e14047f466915cf7fafa8
Valtteri <savonen.emppu@gmail.com>
2023-02-06 18:41:19 +0200

    added ga4

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,28 @@ import '../styles/globals.css'
 
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
+import Script from 'next/script';
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  return(
+    <>
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-35DL2QX4EB`}/>
+      <Script strategy="lazyOnload">
+        {`
+          <!-- Google tag (gtag.js) -->
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-35DL2QX4EB"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-35DL2QX4EB');
+          </script>
+
+        `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+)}
