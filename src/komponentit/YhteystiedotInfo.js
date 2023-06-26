0a6e6e3ab65f79610f78fa401e6b3dc6f6869759
Valtteri Savonen <savonen.emppu@gmail.com>
2023-06-26 16:38:03 +0300

    Update YhteystiedotInfo.js

--- src/komponentit/YhteystiedotInfo.js
+++ src/komponentit/YhteystiedotInfo.js
@@ -1,34 +1,22 @@
-import React from 'react';
+import React from "react";
 
 export const YhteystiedotInfo = () => {
   return (
-    <div className='yhteystiedotInfo'>
-        <h1>Yhteystiedot</h1>
-        
-        <div className='yhteystiedot-row' >
+    <div className="yhteystiedotInfo">
+      <h1>Yhteystiedot</h1>
 
-            <div className='tyontekija1'>
-              
-              <div className='henkiloTiedot'>                
-                <h4>TONI PYSTÖ</h4>
-                <p>Toimitusjohtaja</p>
-                <p>puh: 044 088 3886</p>
-                <small>toni.pysto@vihergia.fi</small>
-              </div>
-            </div>
-
-            <div className='tyontekija2'>
-              <div className='henkiloTiedot'>                
-                <h4>RAVINDER KOHTANIEMI</h4>
-                <p>Työnjohtaja</p>
-                <p>puh: 044 987 3459</p>
-                <small>info@vihergia.fi</small>
-              </div>
-            </div>
-
+      <div className="yhteystiedot-row">
+        <div className="tyontekija1">
+          <div className="henkiloTiedot">
+            <h4>JERE HAIKARAINEN</h4>
+            <p>Toimitusjohtaja</p>
+            <p>puh: 040 4180667</p>
+            <small>jak.haikarainen@gmail.com</small>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default YhteystiedotInfo
+export default YhteystiedotInfo;
